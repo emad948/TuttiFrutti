654649ad84a2d06348d18959cbdccdfba28a4f61
Alexander Montag <mail@alexander-montag.de>
2021-12-15 04:39:09 +0100

    PlayerCharacter.SetColor

--- Assets/Scripts/Character/PlayerCharacter.cs
+++ Assets/Scripts/Character/PlayerCharacter.cs
@@ -8,7 +8,8 @@ using UnityEngine;
 public class PlayerCharacter : NetworkBehaviour
 {
     
-    
+    public Color color;
+    public GameObject michelin;
    [SerializeField] private TMP_Text playerDisplayName;
 
    public string displayName;
@@ -18,7 +19,15 @@ public class PlayerCharacter : NetworkBehaviour
 private void Start()
 {
     if (!hasAuthority)playerDisplayName.text = displayName;
-       
+    setColor(color);
+}
+
+public void setColor(Color color){
+        if (color != null && michelin){
+        foreach(var component in  michelin.GetComponentsInChildren<SkinnedMeshRenderer>()){
+            component.material.color = color;
+        }
+    }
 }
 
 
