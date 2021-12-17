0cc52f80d1a860f93985462f75d4b6bc0e4c8a5d
Colin Kolbe <colinkolbe@outlook.com>
2021-12-17 22:06:30 +0200

    Refactoring, fix stair position in HillKing

---
+++ Assets/Scripts/Other/LevelUI.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+using TMPro;
+
+public class LevelUI : MonoBehaviour
+{
+    public TMP_Text score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score.text = NetworkClient.localPlayer.GetComponent<NetworkPlayer>().GetScore();
+    }
+}
