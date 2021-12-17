39f44b7268fc215687011b109ecda58c7c889656
Colin Kolbe <colinkolbe@outlook.com>
2021-12-17 21:46:32 +0200

    Refactoring, update wiggle button script

--- Assets/Scripts/Menus/WiggleObject.cs
+++ Assets/Scripts/Menus/WiggleObject.cs
@@ -6,7 +6,7 @@ public class WiggleObject : MonoBehaviour
 {
     public float wiggleDistance = 2;
     public float wiggleSpeed = 1;
-
+    public float offset;
     private Vector3 orgPos;
     // set x and y pos in start() to wiggle around
     private void Start()
@@ -16,8 +16,8 @@ public class WiggleObject : MonoBehaviour
  
     void Update()
     {
-        float xPosition = Mathf.Sin(Time.time * wiggleSpeed) * wiggleDistance;
-        float yPosition = Mathf.Cos(Time.time * wiggleSpeed) * wiggleDistance;
+        float xPosition = Mathf.Sin(Time.time * wiggleSpeed + offset) * wiggleDistance;
+        float yPosition = Mathf.Cos(Time.time * wiggleSpeed + offset) * wiggleDistance;
         transform.localPosition = orgPos + new Vector3(xPosition, yPosition, 0);
     }
 }
