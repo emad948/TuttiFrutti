07668a44455fa6d9dab794698a71228dd8b854bc
Alexander Montag <mail@alexander-montag.de>
2021-12-21 10:45:44 +0100

    first step towards external forces

--- Assets/Scripts/Character/ExternalForces.cs
+++ Assets/Scripts/Character/ExternalForces.cs
@@ -5,9 +5,86 @@ using Mirror;
 using TMPro;
 using UnityEngine;
 
-public class ExternalForces : MonoBehaviour
+public class ExternalForces : NetworkBehaviour
 {
-    void OnCollisionEnter(Collider col){
-        print("hallo");
+    private static ExternalForces identity;
+    public static ExternalForces singleton{get{
+        if (identity == null) identity = new ExternalForces();
+        
+        return identity;
+    }
+    }
+    private ExternalForces(){
+        Start();
+    }
+
+
+    [SyncVar] ArrayList globalForces;
+    [SyncVar] ArrayList forces;
+    public float forceMult = 0.01f;
+    public float timeMult = 100;
+
+    public float minThreshold = 0.0001f;
+    public Vector3 force(GameObject g){
+        Vector3 result = Vector3.zero;
+        foreach (Force f in forces){
+            //if (f.target == g)
+             result += f.current;
+        }
+        return result;
+    }
+
+    void Awake(){
+        DontDestroyOnLoad(gameObject);
+    }
+    void Start(){
+        forces = new ArrayList();
+        identity = this;
+    }
+    [Command] void AddOnServer(Force force){forces.Add(force);print("force added on server by client");}
+
+    private void AddForce(Force force){
+        if (isServer) forces.Add(force);
+        else AddOnServer(force);
+    }
+    public void pushCharacter(GameObject target, GameObject source){
+        Vector3 sourceSpeed = source.transform.rotation * Vector3.forward * forceMult;
+        Force newForce = new Force(sourceSpeed, target);
+        AddForce(newForce);
+
+    } 
+    
+    void Update(){
+        foreach (Force force in forces){
+            if (force.final.magnitude - force.current.magnitude < minThreshold){
+                 forces.Remove(force);
+                 Update();
+                 return;
+            }
+            force.current = Vector3.Lerp(force.current, force.final, Time.deltaTime * timeMult);
+            if (forces.Count > 0) print(forces);
+                    globalForces = forces;
+        }
+    }
+
+    
+    private class Force{
+        public Force(Vector3 force, GameObject target){
+            final = force;
+            current = Vector3.zero;
+        }
+
+        public Force(){
+            final = current = Vector3.zero;
+            target = null;
+        }
+        public Vector3 final{get; set;}
+        public Vector3 current{get; set;}
+
+        public GameObject target;
+        public string toString(){
+            return current + " " + final + " " + target.tag;
+        }
     }
 }
+
---
+++ Assets/Scripts/Character/PlayerCollision.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using TMPro;
+using UnityEngine;
+public class PlayerCollision : NetworkBehaviour{
+  
+    void OnControllerColliderHit(ControllerColliderHit hit){
+        if (!hasAuthority) return; // not optimal for many players: use disabled gameObject instead with AuthorityManager
+        var target = hit.gameObject;
+        //print(other.tag);
+        if (target.tag == "PlayerCharacter"){
+            ExternalForces.singleton.pushCharacter(gameObject, target);
+         }
+    }
+}
