e702d87c5cb85a8f34bf79aec4f39bef96c8f6d0
Colin Kolbe <colinkolbe@outlook.com>
2022-01-17 23:09:53 +0200

    Add text-wiggle to MainMenu title

--- Assets/Scripts/Menus/WiggleObject.cs
+++ Assets/Scripts/Menus/WiggleObject.cs
@@ -9,13 +9,13 @@ public class WiggleObject : MonoBehaviour
     public float wiggleDistance = 2;
     public float wiggleSpeed = 1;
     public float offset;
+    public bool isTitleText = false;
+    public float fontSizeFactor = 3.5f;
 
     private TMP_Text text;
     private Vector3 orgPos;
     private float orgFontSize;
-    private float fontSizeFactor = 3.5f;
 
-    // set x and y pos in start() to wiggle around
     private void Start()
     {
         orgPos = transform.localPosition;
@@ -25,10 +25,19 @@ public class WiggleObject : MonoBehaviour
 
     void Update()
     {
-        float xPosition = Mathf.Sin(Time.time * wiggleSpeed + offset) * wiggleDistance;
-        float yPosition = Mathf.Cos(Time.time * wiggleSpeed + offset) * wiggleDistance;
-        transform.localPosition = orgPos + new Vector3(xPosition, yPosition, 0);
-        // 
-        text.fontSize = orgFontSize + Mathf.Sin(Time.time * wiggleSpeed + offset) * fontSizeFactor;
+        if (!isTitleText)
+        {
+            float xPosition = Mathf.Sin(Time.time * wiggleSpeed + offset) * wiggleDistance;
+            float yPosition = Mathf.Cos(Time.time * wiggleSpeed + offset) * wiggleDistance;
+            transform.localPosition = orgPos + new Vector3(xPosition, yPosition, 0);
+        }
+        if (!isTitleText)
+        {
+            text.fontSize = orgFontSize + Mathf.Sin(Time.time * wiggleSpeed + offset) * fontSizeFactor;
+        }
+        else
+        {
+            text.fontSize = orgFontSize + Mathf.Abs(Mathf.Sin(Time.time * wiggleSpeed + offset) * fontSizeFactor);
+        }
     }
 }
