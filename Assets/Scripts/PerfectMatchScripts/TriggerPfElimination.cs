0207e90edaf6514ed6f4f0b3de2fca6fea68f1b1
Alexander Montag <mail@alexander-montag.de>
2022-01-19 17:26:30 +0100

    PerfectMatch: switch to global camera when fallen out

--- Assets/Scripts/PerfectMatchScripts/TriggerPfElimination.cs
+++ Assets/Scripts/PerfectMatchScripts/TriggerPfElimination.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
 
+
 public class TriggerPfElimination : NetworkBehaviour
 {
+
+    public GameObject globalObserverCamera;
 
     public PerfectMatchScoring scoring;
     // Start is called before the first frame update
 
     void OnTriggerEnter(Collider other){
         // Message player
-        
+        if(other.gameObject.GetComponent<PlayerCharacter>().playerHasAuthority) globalObserverCamera.SetActive(true);
+            
         // 
         if(!isServer) return;
         scoring.playerFellOut(other.gameObject);
