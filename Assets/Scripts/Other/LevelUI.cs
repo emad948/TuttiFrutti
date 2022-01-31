a106f8f4b753d822d1d27c9459961bff89f94e12
Colin Kolbe <colinkolbe@outlook.com>
2022-01-31 08:30:24 +0200

    Fix LevelUI for real

--- Assets/Scripts/Other/LevelUI.cs
+++ Assets/Scripts/Other/LevelUI.cs
@@ -13,8 +13,8 @@ public class LevelUI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        var curlevelIndex = ((GameNetworkManager) NetworkManager.singleton).currentLevelIndex.ToString() + 1;
-        levelIndex.text = curlevelIndex + "/4"; 
+        int curlevelIndex = ((GameNetworkManager) NetworkManager.singleton).currentLevelIndex + 1;
+        levelIndex.text = curlevelIndex.ToString() + "/4"; 
     }
 
     // Update is called once per frame
